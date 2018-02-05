ba3210d5ebe6b90a354b2e61faad369980eac93d
soyyo5159 <soyyo5159@hotmail.com>
2018-02-05 14:54:18 -0300

    Ahora se permite descargar los fallecidos en pdf

--- main.js
+++ main.js
@@ -13,4 +13,45 @@ app.use("/",express.static("website"))
 app.use("/fallecidos",require("./fallecidos")(db))
 app.get("/url",require("./myHost"))
 app.get("/repetidos",require("./repetidos")(db))
+
+
+const pdfDocument=require("pdfkit");
+
+app.get("/lista",(req,res)=>{
+    db.find({})
+    .then((todos)=>todos.map(e=>e.nombre))
+    .then(nombres=>nombres.sort((n1,n2)=>{
+        let arreglado1 = n1.split(" ").reverse().join(" ");
+        let arreglado2 = n2.split(" ").reverse().join(" ");
+        if(arreglado1<arreglado2) return -1;
+        else return 1;
+    }))
+    .then(nombres => nombres.map((e,i)=>i+" - "+e))
+    .then(nombresNumerados=>nombresNumerados.join("\n"))
+    .then(texto=>{
+        console.log("Empiezo a hacer el pdf")
+        res.set({"Content-Disposition":"attachment; filename=\"Fallecidos.pdf\""});
+
+        let doc= new pdfDocument();
+        doc.pipe(res);
+        doc.fontSize(25).text(texto);
+        console.log("Antes de end")
+        doc.end();
+        console.log("Despues de end")
+    })
+    /*
+    .then(texto=>pdf.create(texto).toBuffer((err,buffer)=>{
+        console.log("Acá tengo el buffer");
+        res.set({"Content-Disposition":"attachment; filename=\"Fallecidos.pdf\""});
+        res.send(buffer);
+    }))
+    */
+
+    /*
+    .then(texto=>{
+        res.set({"Content-Disposition":"attachment; filename=\"Fallecidos.txt\""});
+        res.send(nombres)
+    })
+    */
+})
 app.listen(3000,()=>console.log("escuchando en 3000"));
